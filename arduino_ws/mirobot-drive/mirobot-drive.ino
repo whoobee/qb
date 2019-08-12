d3f4385b7614fc4b93252da38c255b80f66544b9
Mihai Hubalau <hubalau@gmail.com>
2019-08-12 19:33:06 +0100

    Disabled telemetry data. todo - fix telemetry info + ros msgs

--- arduino_ws/mirobot-drive/mirobot-drive.ino
+++ arduino_ws/mirobot-drive/mirobot-drive.ino
@@ -6,6 +6,11 @@
 /* Threading include */
 #include <Thread.h>
 
+#define E_TRUE             (1)
+#define E_FALSE            (0)
+
+#define EN_TELEMETRY_DATA  E_FALSE
+
 /* ROS include */
 #include <ros.h>
 #include <std_msgs/String.h>
@@ -35,26 +40,31 @@ ros::Subscriber<mirobot_driver::wheel_control> sub("wheel_ctrl", WheelDataCbk );
 /* ROS Publisher */
 void PubCbk(void);
 
+#if (EN_TELEMETRY_DATA == E_TRUE)
 mirobot_driver::bot_telemetry bot_tele;
 ros::Publisher bot_telemetry("bot_telemetry", &bot_tele);
 
 mirobot_driver::wheel_telemetry wheel_tele;
 ros::Publisher wheel_telemetry("wheel_telemetry", &wheel_tele);
-
+#endif  /* #if (EN_TELEMETRY_DATA == E_TRUE) */
 
 /************************/
 /* Makeblock definitions*/
 /************************/
+#if (EN_TELEMETRY_DATA == E_TRUE)
 MeGyro gyro;
-int32_t gyroX;
-int32_t gyroY;
-int32_t gyroZ;
-int32_t accX;
-int32_t accY;
-int32_t accZ;
 
-int32_t gyroRight;
-int32_t gyroLeft;
+int32_t gyroX = 0;
+int32_t gyroY = 0;
+int32_t gyroZ = 0;
+int32_t accX  = 0;
+int32_t accY  = 0;
+int32_t accZ  = 0;
+
+int32_t gyroRight = 0;
+int32_t gyroLeft  = 0;
+#endif  /* (EN_TELEMETRY_DATA == E_TRUE) */
+
 
 MeMegaPiDCMotor rightMotor(PORT1B); /* Right Motor */
 MeMegaPiDCMotor leftMotor(PORT2B);  /* Left Motor */
@@ -67,22 +77,41 @@ int16_t leftMotorSpeed  = 0;
 /* Subscriber Cbk */
 void WheelDataCbk( const mirobot_driver::wheel_control& wheel_data)
 {
-  char tmpbuf [4];
+  char    tmpbuf [4];
+  int16_t dir = 1;
   
-  rightMotorSpeed = wheel_data.speed_r*2;
-  leftMotorSpeed  = wheel_data.speed_l*2;
+  if(wheel_data.dir_r == 0)
+  {
+    dir = 1;
+  }
+  else
+  {
+    dir = -1;
+  }
+  rightMotorSpeed = (dir * (wheel_data.speed_r*2));
 
-  sprintf (tmpbuf, "%03i", rightMotorSpeed);
-  nh.loginfo("RightMotor = ");
-  nh.loginfo(tmpbuf);
-  sprintf (tmpbuf, "%03i", leftMotorSpeed);
-  nh.loginfo("LeftMotor = ");
-  nh.loginfo(tmpbuf);
+  if(wheel_data.dir_l == 0)
+  {
+    dir = 1;
+  }
+  else
+  {
+    dir = -1;
+  }
+  leftMotorSpeed  = (dir * (wheel_data.speed_l*2));
+
+  //sprintf (tmpbuf, "%03i", rightMotorSpeed);
+  //nh.loginfo("RightMotor = ");
+  //nh.loginfo(tmpbuf);
+  //sprintf (tmpbuf, "%03i", leftMotorSpeed);
+  //nh.loginfo("LeftMotor = ");
+  //nh.loginfo(tmpbuf);
 }
 
 /* pubThread Cbk*/
 void PubCbk(void)
 {
+#if (EN_TELEMETRY_DATA == E_TRUE)
   bot_tele.id = 0;
   bot_tele.gyro_x = gyroX;
   bot_tele.gyro_y = gyroY;
@@ -95,28 +124,34 @@ void PubCbk(void)
   wheel_tele.speed_r = gyroRight;
   wheel_tele.speed_l = gyroLeft;
   wheel_telemetry.publish( &wheel_tele );
+#endif  /* #if (EN_TELEMETRY_DATA == E_TRUE) */
 }
 
 /* Init function */
 void setup()
 {
+#if (EN_TELEMETRY_DATA == E_TRUE)
   gyro.begin();
+#endif  /* #if (EN_TELEMETRY_DATA == E_TRUE) */
   pubThread.onRun(PubCbk);
   pubThread.setInterval(500);
   
   nh.initNode();
+#if (EN_TELEMETRY_DATA == E_TRUE)
   nh.advertise(bot_telemetry);
+#endif
   nh.subscribe(sub);
 }
 
 /* Main handler */
 void loop()
 {
+#if (EN_TELEMETRY_DATA == E_TRUE)
   gyro.update();
   gyroX = gyro.getAngleX();
   gyroY = gyro.getAngleY();
   gyroZ = gyro.getAngleZ();
-  
+#endif  /* #if (EN_TELEMETRY_DATA == E_TRUE) */  
   // checks if the publisher thread should run
   if(pubThread.shouldRun())
   {
@@ -125,7 +160,7 @@ void loop()
 
   rightMotor.run(-rightMotorSpeed);   /* value: between -255 and 255. */
   leftMotor.run(leftMotorSpeed);      /* value: between -255 and 255. */
-  
+
   nh.spinOnce();
   delay(100);
 }
