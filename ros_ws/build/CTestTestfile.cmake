aac856c96afbfb6ae7165989be66e27724147759
whoobee <hubalau@gmail.com>
2020-03-14 19:05:52 +0100

    switch to odrive

--- ros_ws/build/CTestTestfile.cmake
+++ ros_ws/build/CTestTestfile.cmake
@@ -6,8 +6,7 @@
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
 subdirs("mirobot_gazebo")
-subdirs("mirobot_scripts/arduino_scripts/arduino_peripherals")
-subdirs("mirobot_scripts/esp32_scripts/esp32_peripherals")
+subdirs("odrive_ros")
 subdirs("mirobot_base")
 subdirs("mirobot_control")
 subdirs("mirobot_navigation")
--- ros_ws/build/catkin_generated/setup_cached.sh
+++ ros_ws/build/catkin_generated/setup_cached.sh
@@ -7,8 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/whoobee/mirobot/ros_ws/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/opt/ros/melodic/lib:/usr/local/lib"
 export PWD="/home/whoobee/mirobot/ros_ws/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/whoobee/mirobot/ros_ws/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/whoobee/mirobot/ros_ws/src:$ROS_PACKAGE_PATH"
