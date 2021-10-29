54e08171f114e0c9deb536bcbbfb9cf0a648793f
whoobee <hubalau@gmail.com>
2021-10-29 20:11:14 +0200

    update install script for noetic

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,22 +9,23 @@ sudo apt-get install python python-pip python3 python3-pip -y
 #ros install and configure
 ##########################
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt install curl # if you haven't already installed curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
-sudo apt install ros-melodic-desktop-full -y
-echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+sudo apt install ros-noetic-desktop-full -y
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
-sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential -y
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
 sudo rosdep init
 rosdep update
 echo "source ~/qb/ros_ws/devel/setup.bash" >> ~/.bashrc
-echo "export ROS_MASTER_URI=http://192.168.0.178:11311" >> ~/.bashrc
-echo "export ROS_HOSTNAME=192.168.0.178" >> ~/.bashrc
-echo "export ROS_IP=192.168.0.178" >> ~/.bashrc
+echo "export ROS_MASTER_URI=http://192.168.0.180:11311" >> ~/.bashrc
+echo "export ROS_HOSTNAME=192.168.0.180" >> ~/.bashrc
+echo "export ROS_IP=192.168.0.180" >> ~/.bashrc
 
 #install qB ROS dependecies
 ################################
-sudo apt-get install -y ros-melodic-map-server ros-melodic-gmapping ros-melodic-rosbridge-server ros-melodic-joy ros-melodic-teleop-twist-joy ros-melodic-web-video-server ros-melodic-amcl ros-melodic-move-base ros-melodic-dwa-local-planner ros-melodic-rosapi ros-melodic-pointcloud-to-laserscan ros-melodic-rtabmap-ros
+sudo apt-get install -y ros-noetic-map-server ros-noetic-gmapping ros-noetic-rosbridge-server ros-noetic-joy ros-noetic-teleop-twist-joy ros-noetic-web-video-server ros-noetic-amcl ros-noetic-move-base ros-noetic-dwa-local-planner ros-noetic-rosapi ros-noetic-pointcloud-to-laserscan ros-noetic-rtabmap-ros
 
 #clone qB and setup
 ########################
@@ -50,7 +51,7 @@ sudo sh ~/qb/ros_ws/src/ydlidar_ros/startup/initenv.sh
 
 #setup realsense cameras
 ########################
-sudo apt-get install ros-melodic-realsense2-camera -y
+sudo apt-get install ros-noetic-realsense2-camera -y
 sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE
 sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main" -u
 sudo apt-get install librealsense2-dkms -y
@@ -65,7 +66,7 @@ catkin_make
 #enable shutdown for all users
 sudo chmod u+s /sbin/shutdown
 #copy custome power button handler
-sudo cp /home/whoobee/qb/script/powerbtn/power /etc/acpi/events/power
+sudo cp /home/whoobee/qb/scripts/powerbtn/power /etc/acpi/events/power
 #install powerbutton listener
 sudo service acpid restart
 sudo cp /home/whoobee/qb/scripts/powerbtn/power_listener.service /lib/systemd/system/
