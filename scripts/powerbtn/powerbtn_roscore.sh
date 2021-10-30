02f87b0e218efb5305cb6457fb5eb4833c10c22a
whoobee <hubalau@gmail.com>
2021-10-30 16:56:10 +0200

    update IP addr for start-up scripts

--- scripts/powerbtn/powerbtn_roscore.sh
+++ scripts/powerbtn/powerbtn_roscore.sh
@@ -2,8 +2,8 @@
 
 source /opt/ros/melodic/setup.bash
 source /home/whoobee/qb/ros_ws/devel/setup.bash
-export ROS_MASTER_URI=http://192.168.0.178:11311
-export ROS_HOSTNAME=192.168.0.178
-export ROS_IP=192.168.0.178
+export ROS_MASTER_URI=http://192.168.0.180:11311
+export ROS_HOSTNAME=192.168.0.180
+export ROS_IP=192.168.0.180
 
 roscore
