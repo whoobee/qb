5d8d419f58a56b5110b84f03e87320ad767e7915
whoobee <hubalau@gmail.com>
2021-11-01 16:39:17 +0100

    fix IP for rosbridge. don't start webserver from qb.launch... it is automatically started at startup

--- ros_ws/src/qb-web-server/src/scripts/start-ros-master.sh
+++ ros_ws/src/qb-web-server/src/scripts/start-ros-master.sh
@@ -2,8 +2,9 @@
 
 source /opt/ros/melodic/setup.bash
 source /home/whoobee/qb/ros_ws/devel/setup.bash
-export ROS_MASTER_URI=http://192.168.0.178:11311
-export ROS_HOSTNAME=192.168.0.178
-export ROS_IP=192.168.0.178
+export QB_IP_ADDR=$(hostname -I | awk '{print $1}')
+export ROS_MASTER_URI=http://${QB_IP_ADDR}:11311
+export ROS_HOSTNAME=${QB_IP_ADDR}
+export ROS_IP=${QB_IP_ADDR}
 
 roslaunch qb_state qb_state.launch
